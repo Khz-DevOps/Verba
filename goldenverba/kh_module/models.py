33e1aa94109a2cd7227498b4e25446a88853db57
Magdy Mousa <MAjdi.M@alkhwarizmi.ai>
2024-10-02 17:09:56 +0300

    fix2

--- goldenverba/kh_module/models.py
+++ goldenverba/kh_module/models.py
@@ -4,23 +4,23 @@ from pydantic import BaseModel, Field
 from typing import List, Optional, Any
 
 class GPTTopic(BaseModel):
-    id: int = Field(0, alias="_id")
-    intent: str = "default_intent"
-    context: str = "default_context"
-    examples: List[str] = []
-    instructions: List[str] = []
-    promptExamples: List[Optional[str]] = []
-    promptId: int = -1
-    test: List[Optional[str]] = []
-    color: str = "unknown"
+    id: Optional[int] = Field(None, alias="_id")
+    intent: Optional[str] = None
+    context: Optional[str] = None
+    examples: Optional[List[str]] = []
+    instructions: Optional[List[str]] = []
+    promptExamples: Optional[List[Optional[str]]] = []
+    promptId: Optional[int] = None
+    test: Optional[List[Optional[str]]] = []
+    color: Optional[str] = None
 
     # Added fields from Chunk
-    content: str = ""
-    chunk_id: int = -1
-    doc_uuid: str = "unknown_uuid"
-    title: str = "Untitled"
-    pca: Any = None  # Replace 'Any' with the appropriate type if known
-    start_i: int = 0
-    end_i: int = 0
-    content_without_overlap: str = ""
-    labels: List[str] = []
+    content: Optional[str] = None
+    chunk_id: Optional[int] = None
+    doc_uuid: Optional[str] = None  # Made Optional
+    title: Optional[str] = None
+    pca: Optional[Any] = None  # Replace 'Any' with the appropriate type if known
+    start_i: Optional[int] = None
+    end_i: Optional[int] = None
+    content_without_overlap: Optional[str] = None
+    labels: Optional[List[str]] = []
